4bb92baecf4ddb8a42598e6c88c964b8c4d63733
momnhuong <93124426+momnhuong@users.noreply.github.com>
2021-10-27 08:56:11 +0700

    Create user2
    This is create user2

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+    access_key = "AKIA2KFKAH3SCBS7E5HB"
+    secret_key = "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"
+    region = "ap-southeast-1"
+}
+resource "aws_iam_user" "user2" {
+    name = "user2"
